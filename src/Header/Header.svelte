28c8421fd8f897f1314535d4e133a5dd46e1ce0e
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-07-16 14:51:16 -0500

    add header nav and logo component

--- src/Header/Header.svelte
+++ src/Header/Header.svelte
@@ -1,1 +1,19 @@
-<p>Header</p>
+<script>
+  import Logo from './Logo/Logo.svelte'
+  import Nav from './Nav/Nav.svelte'
+
+</script>
+
+<header>
+  <Logo />
+  <Nav />
+</header>
+
+<style>
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 20px;
+  }
+</style>
---
+++ src/Header/Nav/Nav.svelte
@@ -0,0 +1,12 @@
+
+<nav>
+  <a href="/about">ABOUT</a>
+  <a href="/contact">CONTACT</a>
+</nav>
+
+<style>
+  a {
+    color: black;
+    margin: 20px;
+  }
+</style>
