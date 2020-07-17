eda8d2e1ecf8172540164933fc41e3f573ea0b9a
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-07-17 14:08:37 -0500

    add sample images for hoime page and arrange in grid

--- src/Home/Home.svelte
+++ src/Home/Home.svelte
@@ -1,1 +1,80 @@
-<p>Home</p>
+<script>
+  import { onMount } from 'svelte'
+
+  let images
+
+  const groupImages = (data) => {
+    let images = []
+    data.forEach((item, i) => {
+      if (i % 2 == 0 ) {
+        images.push([item])
+      } else {
+        images[images.length - 1].push(item)
+      }
+    })
+    console.log(images)
+   return images
+  }
+
+  const getImages = () => {
+    fetch('https://picsum.photos/v2/list')
+    .then(response => response.json())
+    .then(data => images = groupImages(data))
+  } 
+
+  onMount(() => {
+    getImages()
+  })
+</script>
+
+<div class='projects'>
+  {#if images}
+    {#each images as image}
+      <div class='project'>
+        <img src={image[0].download_url} />
+      </div>
+    {/each}
+  {/if}
+</div>
+
+<style>
+  .projects {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    margin-right: calc(2 * var(--huge));
+    margin-bottom: calc(2 * var(--huge));
+    margin-left: calc(150px + (2 * var(--medium)));
+    grid-gap: var(--huge);
+  }
+
+  .project {
+    width: 100%;
+    cursor: pointer;
+  }
+
+  .project::before {
+    content: "";
+    display: inline-block;
+    padding-bottom: 100%;
+  }
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  @media screen and (max-width: 1400px) {
+    .projects {
+      margin-right: var(--huge);
+      margin-bottom: var(--huge);
+    }
+  }
+
+  @media screen and (max-width: 1000px) {
+    .projects {
+      grid-template-columns: repeat(2, 1fr);
+      margin: var(--medium);
+    }
+  }
+</style>
