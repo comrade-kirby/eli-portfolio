c46ba6b094ded0b46fea5ba8954c2ca30ad18192
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-07-27 15:30:34 -0500

    WIP project component

--- src/Project/Project.svelte
+++ src/Project/Project.svelte
@@ -1,1 +1,16 @@
-<p>Project</p>
+<script>
+  import { projects } from '../stores.js'
+
+  export let projectKey
+
+  let project
+
+  $: project = $projects.find( project => project.key == projectKey)
+</script>
+
+{#if project}
+  <h1>{project.name}</h1>
+  {#each project.project_copy as copy}
+    <p>{copy}</p>
+  {/each}
+{/if}
