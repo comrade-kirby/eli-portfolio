f52ba84d0f16208df6ff929075479ec5de0907dd
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-07-28 10:56:19 -0500

    add carousel component

---
+++ src/Project/Carousel/Carousel.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let mediae
+
+  $: console.log(mediae)
+</script>
+
+{#each mediae as media}
+  <p>{media}</p>
+{/each}
+
+<p>this is a carousel</p>
--- src/Project/Project.svelte
+++ src/Project/Project.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Carousel from './Carousel/Carousel.svelte'
   import { projects } from '../stores.js'
 
   export let projectKey
@@ -9,8 +10,20 @@
 </script>
 
 {#if project}
-  <h1>{project.name}</h1>
-  {#each project.project_copy as copy}
-    <p>{copy}</p>
-  {/each}
+  <section class='project'>
+    <h1>{project.name}</h1>
+    <Carousel mediae={project.projectMediae}/>
+    {#each project.projectCopy as copy}
+      <p>{copy}</p>
+    {/each}
+  </section>
 {/if}
+
+<style>
+  .project {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+</style>
