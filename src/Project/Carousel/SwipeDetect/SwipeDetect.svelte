6e7fd0ae1a31c2f2a3054e10175c15995e49a599
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-08-06 15:51:38 -0500

    finess swipe for carousel

--- src/Project/Carousel/SwipeDetect/SwipeDetect.svelte
+++ src/Project/Carousel/SwipeDetect/SwipeDetect.svelte
@@ -1,20 +1,29 @@
 <script>
   export let slide
 
-  let xStart
+  let xStart, yStart
 
-  const touchStart = (event) => xStart = event.touches[0].clientX
-
+  const touchStart = (event) => {
+    xStart = event.touches[0].clientX
+    yStart = event.touches[0].clientY
+  }
+  
   const touchMove = (event) => {
-    if (!xStart) { return }
+    if (!xStart || !yStart) { return }
 
     const xCurrent = event.touches[0].clientX
+    const yCurrent = event.touches[0].clientY
+  
     const xDiff = xStart - xCurrent
+    const yDiff = yStart - yCurrent
     
-    xDiff > 0 ? slide('next') : slide('previous')
-    
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+      xDiff > 0 ? slide('next') : slide('previous')
+    }
+
     xStart = null
   }
+  
 </script>
 
 <div 
@@ -26,8 +35,12 @@
 
 <style>
   .swipe-detect {
+    display: flex;
     min-width:  100%;
     min-height: 100%;
     position: absolute;
+    padding: 0 50px;
+    margin-bottom: 150px;
+    align-self: center;
   }
 </style>
