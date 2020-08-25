03e38b3daebaca4ee4a6554fd582e1cd3474b16e
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-08-25 11:49:20 -0500

    add about page

--- src/About/About.svelte
+++ src/About/About.svelte
@@ -1,1 +1,22 @@
-<section>About</section>
+<section class='about'>
+  <h1 class='name'>ELI CORETTI</h1>
+  <img src='https://pro2-bar-s3-cdn-cf4.myportfolio.com/5553b1484dac46d3f8d6a0a35b178a1c/fdec4804-8194-4e0d-83b1-3b00581f757a_car_1x1.jpeg?h=76c3a530ff75e061bb2135b4226b7bd7' />
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero ipsam explicabo dolorum earum error dolorem, minus quo aliquam maiores ducimus, possimus nulla repudiandae quisquam perferendis ipsum modi? Fugit, sequi accusantium?</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat doloribus soluta magni quod. Sequi maiores sunt velit sapiente cupiditate reiciendis? Odio dolores tenetur magni odit esse, labore veritatis iusto tempore.</p>
+</section>
+
+<style>
+  .about {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: var(--medium-spacing);
+  }
+
+  img {
+    height: 30vh;
+    width: 30vh;
+    object-fit: cover;
+    margin-bottom: var(--large-spacing);
+  }
+</style>
